c186d4dfe5022b7dcc36e3a4590336e78798dd0e
Gaëtan <gae.cathelain@gmail.com>
2020-01-13 13:03:38 +0100

    feat(typescript): setup tsconfig & nodemon & cors

--- src/server.ts
+++ src/server.ts
@@ -1,20 +1,23 @@
 import express from "express";
 import redis from "redis";
+import cors from "cors";
 
 const app = express();
 
-let client = redis.createClient({
+let redisClient = redis.createClient({
   port: 65433, // replace with your port
   host: "127.0.0.1" // replace with your hostanme or IP address
   //    password  : '',    // replace with your password
 });
 
+app.use(cors());
+
 app.get("/", (req, res) => {
-  client.set("lulz", "42", function(err) {
+  redisClient.set("lulz", "42", function(err) {
     if (err) {
       throw err; /* in production, handle errors more gracefully */
     } else {
-      client.get("lulz", function(err, value) {
+      redisClient.get("lulz", function(err, value) {
         if (err) {
           throw err;
         } else {
